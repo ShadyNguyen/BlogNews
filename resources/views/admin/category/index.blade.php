f1137b04167a7bb0b26b4756c347c084bcc7f545
ShadyNguyen <nguyenbinhduong26@gmail.com>
2024-02-19 16:03:31 +0700

    sửa lại tên lần 2

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -26,7 +26,7 @@
                                 <path d="M12 5l0 14" />
                                 <path d="M5 12l14 0" />
                             </svg>
-                            Create
+                            Create Category
                         </a>
                         <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
                             data-bs-target="#modal-report" aria-label="Create new report">
